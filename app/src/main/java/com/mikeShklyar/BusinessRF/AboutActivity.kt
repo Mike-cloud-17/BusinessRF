e117eef30bd5bab7bca6efd8f5e55beb05d69105
Mike_cloud_17 <mishaucheba69@gmail.com>
2023-04-25 01:58:57 +0300

    Full project has been uploaded

---
+++ app/src/main/java/com/mikeShklyar/BusinessRF/AboutActivity.kt
@@ -0,0 +1,23 @@
+package com.mikeShklyar.BusinessRF
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mikeShklyar.BusinessRF.databinding.ActivityAboutBinding
+
+class AboutActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(MainActivity.currentThemeNav[MainActivity.themeIndex])
+        binding = ActivityAboutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.title = "About"
+        binding.aboutText.text = aboutText()
+    }
+    private fun aboutText(): String{
+        return "Developed By: Shklyar Mike" +
+                "\n\nIf you want to provide feedback, I will love to hear that."
+    }
+}
